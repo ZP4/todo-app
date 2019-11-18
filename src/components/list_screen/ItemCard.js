9c659d9d00be48d2a71d96e58d664e41f0087492
ZP4 <jasonhuzheming9@gmail.com>
2019-11-18 02:02:05 -0500

    Added remove item stoppropagation

--- src/components/list_screen/ItemCard.js
+++ src/components/list_screen/ItemCard.js
@@ -3,11 +3,12 @@ import { Button } from 'react-materialize';
 import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
 import {firestoreConnect} from "react-redux-firebase";
 import {connect} from 'react-redux';
+import { compose } from "redux";
 
 class ItemCard extends React.Component {
     handled = (e) => {
         e.preventDefault();
-        console.log(this.props.item.id);
+        console.log(this.props.item.key);
         console.log(this.props.todoList.id);
     };
 
@@ -20,8 +21,22 @@ class ItemCard extends React.Component {
     };
 
     deleteItem = (e) => {
+        e.preventDefault();
         const firestore = this.props.firestore;
-
+        let itemArray;
+        firestore.collection('todoLists').doc(this.props.todoList.id).get()
+            .then((e) => {
+                itemArray = e.get('items');
+                for(let x = 0; x < itemArray.length; x++) {
+                    if(itemArray[x].key === this.props.item.key) {
+                        itemArray.splice(x, 1);
+                    }
+                }
+            }).finally((e) => {
+            firestore.collection('todoLists').doc(this.props.todoList.id).update({
+                items: itemArray
+            });
+        })
     };
 
     render() {
@@ -37,15 +52,16 @@ class ItemCard extends React.Component {
                     <div className="card-content right">
                         <Button onClick={this.handled}>Up</Button>
                         <Button onClick={this.handled}>DOwn</Button>
-                        <Button onClick={this.handled}>Remove</Button>
+                        <Button onClick={this.deleteItem}>Remove</Button>
                     </div>
                 </div>
             </div>
         );
     }
 }
+
 export default compose(
-    connect(mapStateToProps),
+    connect(null),
     firestoreConnect([
         {collection: 'todoLists'},
     ]),
